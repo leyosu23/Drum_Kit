f61c493c627f32ade2659aab009d64fcf9cdd622
leyosu23 <leyosu23@gmail.com>
2021-09-29 18:13:31 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,68 @@
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrumButtons; i++) {
+    //add "click" event all of button with drum class for iterating each loop
+    // if click the first button, event function, attached to the first buttion, will be trigered
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    });
+
+
+}
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default: console.log(key);
+    }
+}
+
+function buttonAnimation(key) {
+    var currentKey = document.querySelector("." + key);
+    currentKey.classList.add("pressed");
+    setTimeout(function () {
+        currentKey.classList.remove("pressed");
+    }, 150)
+}
